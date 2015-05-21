ee8ea686725c58193114440cf33bb628d2da6f39
Michael <meller@navinet.net>
2015-05-21 08:57:14 +0100

    Initial commit, colours all text purple

---
+++ VariableFormat.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VisualBot
+{
+    #region Format definition
+
+    /// <summary>
+    /// Defines an editor format for the VisualBot type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "Variable")]
+    [Name("Variable")]
+    [UserVisible(true)] //this should be visible to the end user
+    [Order(Before = Priority.Default)] //set the priority to be after the default classifiers
+    internal sealed class VariableFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "VisualBot" classification type
+        /// </summary>
+        public VariableFormat()
+        {
+            DisplayName = "Variable"; //human readable version of the name
+            BackgroundColor = Colors.BlueViolet;
+            TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+
+    #endregion
+}
---
+++ VariableType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VisualBot
+{
+    internal static class VariableClassificationDefinition
+    {
+        /// <summary>
+        /// Defines the "VisualBot" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("Variable")]
+        internal static ClassificationTypeDefinition VariableType = null;
+    }
+}
